24258714a1f8b18effa825934890ec35d890fd4e
Celestine <ejioforcelestine77@gmail.com>
2024-04-28 14:31:26 +0100

    changed domain for sending emails

--- src/common/utils/resend.js
+++ src/common/utils/resend.js
@@ -6,13 +6,14 @@ const client = new Resend(resendApiKey)
 // Function to send email with template
 export const sendEmail = async (to, subject, template) => {
     try {
-        await client.emails.send({
-            from: 'noreply@willbethere.com', // Replace with your sender email
+        const email = await client.emails.send({
+            from: 'onboarding@resend.dev', // Replace with your sender email
             to: to,
             subject: subject,
             html: template,
         })
         console.log(`Email sent to ${to}`)
+        console.log(email)
     } catch (error) {
         console.error('Error sending email:', error)
     }
