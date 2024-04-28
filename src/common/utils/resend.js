ab52e990d4c1b58d33b07d7cebca6a1803ad5ca9
Celestine <ejioforcelestine77@gmail.com>
2024-04-28 14:00:37 +0100

    fixed error in sending mail

--- src/common/utils/resend.js
+++ src/common/utils/resend.js
@@ -6,7 +6,7 @@ const client = new Resend(resendApiKey)
 // Function to send email with template
 export const sendEmail = async (to, subject, template) => {
     try {
-        await client.send({
+        await client.emails.send({
             from: 'noreply@willbethere.com', // Replace with your sender email
             to: to,
             subject: subject,
