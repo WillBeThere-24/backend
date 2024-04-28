45e38ad6f2ba5f6672704abc1767f2afe698e2fb
Abdullah <abdullah_busari@yahoo.com>
2024-04-28 08:23:49 +0100

    event item

--- src/modules/routes/event.route.js
+++ src/modules/routes/event.route.js
@@ -1,6 +1,5 @@
 import { Router } from 'express'
-import { createEvent, myEvents, getEvent, inviteGuest, eventGuests, editEvent, deleteEvent } from '../controllers/event.controller.js'
-// import { createEvent, myEvents, getEvent, inviteGuest, eventGuests, editEvent, deleteEvent, addEventItem, getEventItems, toggleShowEventItem, deleteEventItem } from '../controllers/event.controller.js'
+import { createEvent, myEvents, getEvent, inviteGuest, eventGuests, editEvent, deleteEvent, addEventItem, getEventItems, toggleShowEventItem, deleteEventItem } from '../controllers/event.controller.js'
 import guestRouter from './guest.route.js'
 import { protect } from "../../common/middlewares/protect.js";
 
@@ -15,10 +14,10 @@ eventRouter.post('/:id/invite', inviteGuest)
 eventRouter.get('/:id/guests', eventGuests)
 eventRouter.patch('/:id', editEvent)
 eventRouter.delete('/:id', deleteEvent)
-// eventRouter.post('/:id/item', addEventItem)
-// eventRouter.get('/:id/item', getEventItems)
-// eventRouter.post('/:event/toggle/item/:id', toggleShowEventItem)
-// eventRouter.delete('/:event/item/:id', deleteEventItem)
+eventRouter.post('/:id/item', addEventItem)
+eventRouter.get('/:id/item', getEventItems)
+eventRouter.post('/:event/toggle/item/:id', toggleShowEventItem)
+eventRouter.delete('/:event/item/:id', deleteEventItem)
 
 eventRouter.use('/rsvp', guestRouter)
 
