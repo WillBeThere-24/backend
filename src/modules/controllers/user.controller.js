d9484a15a969ba387154efddf7181ae1d6f696fc
Abdullah <abdullah_busari@yahoo.com>
2024-04-28 01:13:44 +0100

    WIP

--- src/modules/controllers/user.controller.js
+++ src/modules/controllers/user.controller.js
@@ -2,14 +2,23 @@ import { catchAsync } from '../../common/utils/errorHandler.js'
 import { UserModel } from '../schemas/user.schema.js'
 import { AppResponse } from '../../common/utils/appResponse.js'
 import { UserEntityTransformer } from '../../common/transformers/entityTransformer.js';
+import { GuestModel } from '../schemas/guest.schema.js';
+import { EventModel } from '../schemas/event.schema.js';
 
 export const getUser = catchAsync(async (req, res) => {
     const { user } = req;
+    const events = EventModel.countDocuments({"user": user})
+    const rsvps = GuestModel.find({"email": user.email}).count
+
     return AppResponse(
       res,
       200,
       "Current user fetched successfully",
-      UserEntityTransformer(user),
+      {
+        "user": UserEntityTransformer(user),
+        "events": events,
+        "rsvps": rsvps
+      },
     );
 });
 
