f82053a72f2cba9dbf16512742e3214cc4324ded
Celestine <ejioforcelestine77@gmail.com>
2024-04-28 08:21:02 +0100

    remove destructure on body

---
+++ src/modules/dtos/auth/signup.dto.js
@@ -0,0 +1,14 @@
+// // dto
+// import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator'
+// import { LoginDto } from './login.dto'
+
+// export class SignupDto extends LoginDto {
+//     constructor(body) {
+//         super(body)
+//         this.assignBody()
+//     }
+
+//     @IsNotEmpty()
+//     @IsBoolean({ message: 'The terms and conditions is required' })
+//     isTermsAndConditionAccepted
+// }
